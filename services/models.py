2202b11f0c95bc843696cd5419d23ffc1f0e4fe0
RicardoIT-Web <rick_piedade@hotmail.com>
2022-06-14 10:58:06 +0000

    Add service models and admin settings for service app

--- services/models.py
+++ services/models.py
@@ -1,23 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Category(models.Model):
     name = models.CharField(max_length=254)
-    display_name = models.CharField(max_length=254, null=True, blank=True)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
         return self.name
 
-    def get_display_name(self):
-        return self.display_name
+    def get_friendly_name(self):
+        return self.friendly_name
 
 
 class Service(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
-    reference = models.CharField(max_length=254, null=True, blank=True)
-    name = models.CharField(max_length=254)
-    description = models.TextField()
+    TYPES = (
+        ('Civil Engineering Structures', 'CES'),
+        ('External Fire Risk Inspections', 'EFRI'),
+        ('Domestic & Commercial Surveys', 'D&CS'),
+    )
+    name = models.CharField(max_length=50, choices=TYPES)
+    includes = models.CharField(max_length=100)
+    description = models.CharField(max_length=254)
     price = models.DecimalField(max_digits=6, decimal_places=2)
+    spotters = models.DecimalField(max_digits=6, decimal_places=2, default=150)
 
     def __str__(self):
         return self.name
