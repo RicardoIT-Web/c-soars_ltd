f0acfa515ef809db9533adaa40af424c82f863ba
RicardoIT-Web <rick_piedade@hotmail.com>
2022-06-20 15:31:56 +0000

    Add styling to main-title background in smaller screens and add services link & icon to smaller screens

--- services/urls.py
+++ services/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.all_services, name='services'),
+    path('<service_id>', views.service_detail, name='service_detail'),
 ]
--- services/views.py
+++ services/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Service
 
 
@@ -12,3 +12,15 @@ def all_services(request):
     }
 
     return render(request, 'services/services.html', context)
+
+
+def service_detail(request, service_id):
+    """ View to show individual services"""
+
+    service = get_object_or_404(Service, pk=service_id)
+
+    context = {
+        'service': service,
+    }
+
+    return render(request, 'services/service_detail.html', context)
