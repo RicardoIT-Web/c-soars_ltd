a55bfbc54470447a448a56806d4ea37603b595c9
RicardoIT-Web <rick_piedade@hotmail.com>
2022-06-21 15:03:01 +0000

    Add add_to_briefcase features

--- briefcase/urls.py
+++ briefcase/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.view_briefcase, name='view_briefcase')
+    path('', views.view_briefcase, name='view_briefcase'),
+    path('add/<item_id>/', views.add_to_briefcase, name='add_to_briefcase'),
 ]
--- briefcase/views.py
+++ briefcase/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def view_briefcase(request):
     """ View to render briefcase page to show services booked"""
 
     return render(request, 'briefcase/briefcase.html')
+
+def add_to_briefcase(request, item_id):
+    """ add qty of specified service to briefcase """
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    briefcase = request.session.get('briefcase', {})
+
+    if item_id in list(briefcase.keys()):
+        briefcase[item_id] += quantity
+    else:
+        briefcase[item_id] = quantity
+
+    request.session['briefcase'] = briefcase
+    print(request.session['briefcase'])
+    return redirect(redirect_url)
