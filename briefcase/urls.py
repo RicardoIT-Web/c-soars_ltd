b30a199e868db7835945f6729394eb841c7a9a33
RicardoIT-Web <rick_piedade@hotmail.com>
2022-06-23 16:45:26 +0000

    Add remove from briefcase functionality

--- briefcase/urls.py
+++ briefcase/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.view_briefcase, name='view_briefcase'),
     path('add/<item_id>/', views.add_to_briefcase, name='add_to_briefcase'),
     path('adjust/<item_id>/', views.adjust_briefcase, name='adjust_briefcase'),
+    path('remove/<item_id>/', views.remove_from_briefcase, name='remove_from_briefcase'),
 ]
--- briefcase/views.py
+++ briefcase/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 
 def view_briefcase(request):
@@ -38,16 +38,15 @@ def adjust_briefcase(request, item_id):
     return redirect(reverse('view_briefcase'))
 
 
-# def remove_from_briefcase(request, item_id):
-#     """ adjust qty of specified service to briefcase """
+def remove_from_briefcase(request, item_id):
+    """ remove item of specified service from briefcase """
 
-#     quantity = int(request.POST.get('quantity'))
-#     briefcase = request.session.get('briefcase', {})
+    try:
+        briefcase = request.session.get('briefcase', {})
+        briefcase.pop(item_id)
 
-#     if quantity > 0:
-#         briefcase[item_id] = quantity
-#     else:
-#         briefcase.pop(item_id)
+        request.session['briefcase'] = briefcase
+        return HttpResponse(status=200)
 
-#     request.session['briefcase'] = briefcase
-#     return redirect(reverse('view_briefcase'))
+    except Exception as e:
+        return HttpResponse(status=500)
