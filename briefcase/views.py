fe74a7f42c00ec45c1e33a7f9dd19b35601a5705
RicardoIT-Web <rick_piedade@hotmail.com>
2022-06-27 16:05:09 +0000

    Initiate service summary page

--- briefcase/views.py
+++ briefcase/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
 from services.models import Service
 
@@ -12,13 +12,14 @@ def view_briefcase(request):
 def add_to_briefcase(request, item_id):
     """ add qty of specified service to briefcase """
 
-    service = Service.objects.get(pk=item_id)
+    service = get_object_or_404(Service, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     briefcase = request.session.get('briefcase', {})
 
     if item_id in list(briefcase.keys()):
         briefcase[item_id] += quantity
+        messages.success(request, f'Updated {service.description}  to quantity of {briefcase[item_id]}')
     else:
         briefcase[item_id] = quantity
         messages.success(request, f'{service.description} added to your briefcase')
@@ -30,13 +31,16 @@ def add_to_briefcase(request, item_id):
 def adjust_briefcase(request, item_id):
     """ adjust qty of specified service to briefcase """
 
+    service = get_object_or_404(Service, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     briefcase = request.session.get('briefcase', {})
 
     if quantity > 0:
         briefcase[item_id] = quantity
+        messages.success(request, f'Updated {service.description}  to quantity of {briefcase[item_id]}')
     else:
         briefcase.pop(item_id)
+        messages.success(request, f'{service.description} removed from your briefcase')
 
     request.session['briefcase'] = briefcase
     return redirect(reverse('view_briefcase'))
@@ -46,11 +50,14 @@ def remove_from_briefcase(request, item_id):
     """ remove item of specified service from briefcase """
 
     try:
+        service = get_object_or_404(Service, pk=item_id)
         briefcase = request.session.get('briefcase', {})
         briefcase.pop(item_id)
+        messages.success(request, f'{service.description} removed from your briefcase')
 
         request.session['briefcase'] = briefcase
         return HttpResponse(status=200)
 
     except Exception as e:
+        messages.error(request, f'Error removing service: {e}')
         return HttpResponse(status=500)
