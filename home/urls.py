2f8cc43148cce60c3de98aed3cd518b5c01976bf
RicardoIT-Web <rick_piedade@hotmail.com>
2022-06-27 14:04:28 +0000

    Attempt to make user reviews work

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path('user_review/<int:service_name>/', views.user_review, name='user_review')
 ]
