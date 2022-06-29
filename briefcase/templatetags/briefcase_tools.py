f522b25e5275f3377ab0fdc08cc90c6e5e813ef0
RicardoIT-Web <rick_piedade@hotmail.com>
2022-06-29 17:21:51 +0000

    Add further styling to payment page to align form to center

--- briefcase/templatetags/briefcase_tools.py
+++ briefcase/templatetags/briefcase_tools.py
@@ -3,6 +3,12 @@ from django import template
 
 register = template.Library()
 
+
 @register.filter(name='calc_subtotal')
 def calc_subtotal(price, quantity):
     return price * quantity
+
+
+@register.filter(name='calc_subtotal2')
+def calc_subtotal2(total_price, quantity):
+    return total_price * quantity
