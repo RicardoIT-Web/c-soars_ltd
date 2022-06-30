87d538964c63b9cb40c191ae6578316c7f930b99
RicardoIT-Web <rick_piedade@hotmail.com>
2022-06-30 21:50:42 +0000

    Install django countries and adjust model to use dropdown list options

--- payment/forms.py
+++ payment/forms.py
@@ -23,15 +23,15 @@ class OrderForm(forms.ModelForm):
             'post_code': 'Post Code',
             'city': 'City',
             'county': 'County',
-            'country': 'Country',
         }
 
         self.fields['name'].widget.attrs['autofocus'] = True
         for field in self.fields:
-            if self.fields[field].required:
-                placeholder = f'{placeholders[field]} *'
-            else:
-                placeholder = placeholders[field]
-            self.fields[field].widget.attrs['placeholder'] = placeholder
+            if field != 'country':
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
             self.fields[field].widget.attrs['class'] = 'stripe-style-input'
             self.fields[field].label = False
--- payment/models.py
+++ payment/models.py
@@ -4,6 +4,8 @@ from django.db import models
 from django.db.models import Sum
 from django.conf import settings
 
+from django_countries.fields import CountryField
+
 from services.models import Service
 
 
@@ -18,7 +20,7 @@ class Order(models.Model):
     post_code = models.CharField(max_length=20, null=True, blank=True)
     city = models.CharField(max_length=40, null=False, blank=False)
     county = models.CharField(max_length=80, null=True, blank=True)
-    country = models.CharField(max_length=40, null=False, blank=False)
+    country = CountryField(blank_label='Country *', null=False, blank=False)
     date = models.DateTimeField(auto_now_add=True)
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
