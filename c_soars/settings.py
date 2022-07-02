f90f2a751947d5fe7e0e0b4a8c4de593112592ba
RicardoIT-Web <rick_piedade@hotmail.com>
2022-07-02 22:11:23 +0000

    Continue setup of stripe payment feature

--- c_soars/settings.py
+++ c_soars/settings.py
@@ -169,6 +169,10 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+# stripe
+STRIPE_CURRENCY = 'gbp'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- payment/views.py
+++ payment/views.py
@@ -1,20 +1,39 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 from .forms import OrderForm
+from briefcase.content import briefcase_content
+import stripe
 
 
 def payment(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     briefcase = request.session.get('briefcase', {})
     if not briefcase:
         messages.error(request, "Your Briefcase is currently empty")
         return redirect(reverse('services'))
 
+    current_purchase = briefcase_content(request)
+    total = current_purchase['grand_total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY
+    )
+
     form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warnng(request, 'Missing Stripe Public Key')
+
     template = 'payment/payment.html'
     context = {
         'form': form,
-        'stripe_public_key': 'pk_test_51L2aidB28RWqcdkUOHQyzryg2UxBKx1F1cBowy4aPlXzqJ7phD1cx45YSx2lp0PObM9rsOwlbyznRCgilTsTFbkb006blWp7tb',
-        'client_secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
