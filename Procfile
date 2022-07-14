ba0fdb442335ef493629b19e9a65751cd7c6a1d6
RicardoIT-Web <rick_piedade@hotmail.com>
2022-07-14 21:57:40 +0000

    Setup gunicorn, procfile and heroku, localhost to settings.py

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn c_soars.wsgi:application
--- c_soars/settings.py
+++ c_soars/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-%*)0@+whi!xmrl-)xt5p(un9)*^kiw4$m+vg)n-7-lh$51)wrf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['c-soars.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -121,12 +121,17 @@ WSGI_APPLICATION = 'c_soars.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASE = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
